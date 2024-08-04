760048102844fc9d30d77fac94c52c072a6af02d
Pranav <pranavjha0002@gmail.com>
2024-08-05 00:11:09 +0530

    Update hardhat.config.js
    Commit 01 
    
    Removing & Revoking Private key from the repo

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/",
-      accounts: ["00f4b892a97f514e2d42f5ff3bcb1fa103f5d504576379159a540c635d4d84cc"], //Your private key starting with "0x"
+      accounts: ["Private Key here"], //Your private key starting with "0x"
     },
   },
 };
